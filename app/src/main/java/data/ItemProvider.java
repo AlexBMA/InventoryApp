9d823b2e06c333f8950cf19d792b25f58c4a719a
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-06-08 19:16:09 +0300

    update 4

---
+++ app/src/main/java/data/ItemProvider.java
@@ -0,0 +1,61 @@
+package data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Alexandru on 6/8/2017.
+ */
+
+public class ItemProvider extends ContentProvider {
+
+    private ItemDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new ItemDbHelper(getContext());
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        return 0;
+    }
+}
