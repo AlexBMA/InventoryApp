97099812146a4792a041b44e3e1f5962f2e9156b
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-06-17 18:26:02 +0300

    update 7

--- app/src/main/java/com/example/alexandru/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/alexandru/inventoryapp/CatalogActivity.java
@@ -1,9 +1,10 @@
 package com.example.alexandru.inventoryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
 
 
 public class CatalogActivity extends AppCompatActivity {
@@ -13,7 +14,15 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        FloatingActionButton fab;
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.button_add);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
