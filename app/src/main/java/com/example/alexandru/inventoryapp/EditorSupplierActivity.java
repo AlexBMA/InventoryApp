56b8d87f5b85e175b2354ca6f10ba257ef356bfc
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-07-08 13:49:52 +0300

    update 45

--- app/src/main/java/com/example/alexandru/inventoryapp/EditorSupplierActivity.java
+++ app/src/main/java/com/example/alexandru/inventoryapp/EditorSupplierActivity.java
@@ -2,12 +2,20 @@ package com.example.alexandru.inventoryapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class EditorSupplierActivity extends AppCompatActivity {
+
+    private EditText nameSupplier;
+    private EditText emailSupplier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editor_supplier);
+        setContentView(R.layout.activity_supplier_editor);
+
+
+        nameSupplier = (EditText) findViewById(R.id.edit_text_name_supplier);
+        emailSupplier = (EditText) findViewById(R.id.edit_text_email_supplier);
     }
 }
