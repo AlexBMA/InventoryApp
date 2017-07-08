79c61a60e7f7ecab0062f7ac6f0f9f59637761f9
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-07-08 14:00:27 +0300

    update 47

---
+++ app/src/main/java/data/SupplierDAOImpl.java
@@ -0,0 +1,51 @@
+package data;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
+
+import model.Supplier;
+
+/**
+ * Created by Alexandru on 7/8/2017.
+ */
+
+public class SupplierDAOImpl implements SupplierDAO<Supplier> {
+
+    @Override
+    public void deleteAllItems(ContentResolver contentResolver, Uri uri) {
+        contentResolver.delete(uri, null, null);
+    }
+
+    @Override
+    public void deleteItem(ContentResolver contentResolver, Uri uri, long id) {
+        String selection = InventoryContact.ItemEntry._ID + " = ?";
+        String[] selectionArgs = {id + ""};
+        contentResolver.delete(uri, selection, selectionArgs);
+    }
+
+    @Override
+    public Uri insertItem(ContentResolver contentResolver, Uri uri, Supplier supplier) {
+
+        ContentValues values = new ContentValues();
+        transformItemInValues(supplier, values);
+        Uri newItemUri = contentResolver.insert(uri, values);
+
+        return newItemUri;
+    }
+
+    @Override
+    public void updateItem(ContentResolver contentResolver, Uri uri, long id, Supplier supplier) {
+
+        ContentValues values = new ContentValues();
+        transformItemInValues(supplier, values);
+        String selection = InventoryContact.ItemEntry._ID + " = ?";
+        String[] selectionArgs = {id + ""};
+        contentResolver.update(uri, values, selection, selectionArgs);
+
+    }
+
+    private void transformItemInValues(Supplier supplier, ContentValues values) {
+
+    }
+}
